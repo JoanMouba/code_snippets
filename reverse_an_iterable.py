8a6927df7f73823e74b51704c07ce7c0671fa8be
JoanMouba <43171207+JoanMouba@users.noreply.github.com>
2022-01-27 05:14:03 +0100

    JM: easy pythonic way to reverse an iterable
    Reverse a string, a list, ...

---
+++ reverse_an_iterable.py
@@ -0,0 +1,10 @@
+#  @uthor: Dr.-Ing. Joan MOUBA, joan.mouba@gmail.com
+# Reverse an iterable in Python: list, string, ...
+
+#thought = "gnihtyna od nac I"
+thought = ["Anything", "do", "can", "I"]
+reversed_thought = thought[::-1]
+
+print("Reversed thought is: ", reversed_thought)
+
+
